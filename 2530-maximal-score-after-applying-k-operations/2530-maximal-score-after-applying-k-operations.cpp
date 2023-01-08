ea5dcd361d42ffc0d2e0039ce91ad761aa0b5bc5
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-01-08 12:48:14 +0530

    Time: 388 ms (88.89%), Space: 75.9 MB (44.44%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     long long maxKelements(vector<int>& nums, int k) {
         
-        
         priority_queue<int>pq;
         
         for(int i=0;i<nums.size();i++)
@@ -10,25 +9,20 @@ public:
             pq.push(nums[i]);
         }
         
-        // if(k == )
-        
-        
         long long sum = 0;
         
-        while(pq.empty() != true && k != 0)
+        while(pq.empty() != true && k>0)
         {
             long long res = pq.top();
-            
             pq.pop();
             
             sum = sum + res;
             
             long double y = res;
-            
-            res = ceil(y/3);
+            y = ceil(y/3);
+            res = y;
             
             pq.push(res);
-            
             k--;
         }
         
