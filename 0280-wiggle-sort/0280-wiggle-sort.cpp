e0df2dd43196d5153c3918cbdfc1d3a243834210
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-01-23 20:11:09 +0530

    Time: 35 ms (5.15%), Space: 13.7 MB (42.96%) - LeetHub

--- 0280-wiggle-sort/0280-wiggle-sort.cpp
+++ 0280-wiggle-sort/0280-wiggle-sort.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    
     void wiggleSort(vector<int>& nums) {
-       
+        
         sort(nums.begin(),nums.end());
         
-        for(int i=1;i<nums.size()-1;i += 2)
+        for(int i=1;i<nums.size()-1;i++)
         {
             swap(nums[i],nums[i+1]);
+            i++;
         }
     }
 };
