27bac724d3ef8d90b3721c56e21ed97b50059392
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-01-24 17:13:31 +0530

    Time: 4 ms (77.42%), Space: 9.1 MB (35.73%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int twoSumLessThanK(vector<int>& nums, int k) {
-        
+     
         sort(nums.begin(),nums.end());
         
-        int i,j,res=-1;
+        int i,j,ans=-1;
         
         i=0;
         j=nums.size()-1;
         
-        while(i < j)
+        while(i<j)
         {
-            if(nums[i] + nums[j] == k)
+            if(nums[i]+nums[j] < k)
             {
-                j--;
-            }
-            
-            else if(nums[i] + nums[j] < k)
-            {
-                res = max(res,nums[i] + nums[j]);
+                ans = max(ans,nums[i]+nums[j]);
                 i++;
             }
             
@@ -28,6 +23,6 @@ public:
             }
         }
         
-        return res;
+        return ans;
     }
 };
