ea581b15b9c679d224a43ef1ac01ab975998c8e9
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-01-26 22:14:16 +0530

    Time: 1386 ms (13.51%), Space: 237.5 MB (5.94%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -5,89 +5,87 @@ public:
     
     int Merge(vector<int>& nums,int start,int end)
     {
-        vector<int>temp;
-        
-        int mid = (start + end)/2;
-        
+        int mid = (start+end)/2;
+         
         int i,j;
-        
+         
         i = start;
         j = mid + 1;
         
         while(i <= mid && j <= end)
         {
-           long long t = 2*(long(nums[j]));
+            long long t = 2*(long(nums[j]));
             
-           while(i <= mid && nums[i] <= t)
-           {
-               i++;
-           }
-            
-           cnt = cnt + (mid-i+1);
-           
-           j++; 
-        }
-        
-        i = start;
-        j = mid + 1;
-        
-        while(i <= mid && j <= end)
-        {
-            if(nums[i] >= nums[j])
+            while(i<= mid && nums[i] <= t)
             {
-                temp.push_back(nums[j]);
-                j++;
+                i++;
             }
             
-            else
+            cnt = cnt + (mid-i+1);
+            j++;
+        }
+        
+        vector<int>temp;
+        
+        i = start;
+        j = mid+1;
+        
+        while(i <= mid && j <= end)
+        {
+            if(nums[i] <= nums[j])
             {
                 temp.push_back(nums[i]);
                 i++;
             }
+            
+            else
+            {
+                temp.push_back(nums[j]);
+                j++;
+            }
         }
         
-         while(i <= mid)
-         {
-              temp.push_back(nums[i]);
+        while(i <= mid)
+        {
+            temp.push_back(nums[i]);
                 i++;
-         }
+        }
         
-         while(j <= end)
-         {
-             temp.push_back(nums[j]);
-                j++;
-         }    
+        while(j <= end)
+        {
+           temp.push_back(nums[j]);
+           j++; 
+        }
         
         int k = 0;
         
-        for(int idx=start;idx<=end;idx++)
+        for(int p = start ; p <= end ; p++)
         {
-            nums[idx] = temp[k];
+            nums[p] = temp[k];
             k++;
         }
         
         return cnt;
     }
-
+    
     void ModifiedMergeSort(vector<int>& nums,int start,int end)
     {
         if(start >= end)
         {
             return;
         }
+        
         int mid = (start + end)/2;
         
-        ModifiedMergeSort(nums,start,mid);
-        ModifiedMergeSort(nums,mid+1,end);
-        Merge(nums,start,end);
-        
-        
+         ModifiedMergeSort(nums,start,mid);
+         ModifiedMergeSort(nums,mid+1,end);
+         Merge(nums,start,end);
     }
     
     int reversePairs(vector<int>& nums) {
         
-       ModifiedMergeSort(nums,0,nums.size()-1);
+        ModifiedMergeSort(nums,0,nums.size()-1);
         
-       return cnt; 
+        return cnt;
     }
 };
