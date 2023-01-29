8a2962f3aa939d3d4f6a772b3ab5829d9be3e2b2
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-01-29 15:07:00 +0530

    Time: 23 ms (77.27%), Space: 14.3 MB (32.36%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
     
-     void dfs(int u, vector<int>& vis,vector<vector<int>>& adj) 
-     {
-        vis[u] = 1;
-
-        for (auto v : adj[u])
+    void dfs(int node,vector<int>& vis,vector<vector<int>>&adj)
+    {
+        vis[node] = 1;
+        
+        for(auto it : adj[node])
         {
-            if (!vis[v])
+            if(!vis[it])
             {
-                dfs(v, vis, adj);
+                dfs(it,vis,adj);
             }
         }
     }
-
-    int countComponents(int n, vector<vector<int>>& edges) 
-    {
-        vector<int> vis(n, 0);
-        // let's convert edges into adj list
-        vector<vector<int>> adj;
-        adj.resize(n, vector<int>());
-
-        for (int i = 0; i < edges.size(); ++i) {
+    int countComponents(int n, vector<vector<int>>& edges) {
+        
+        vector<vector<int>>adj(n);
+        
+        vector<int>vis(n,0);
+        
+        int cnt = 0;
+        
+        for(int i=0;i<edges.size();i++)
+        {
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
         
-        int ans = 0;
-
-        for (int i = 0; i < n; ++i) {
-            if (!vis[i]) {
-                dfs(i, vis, adj);
-                ++ans;
+        for(int i=0;i<vis.size();i++)
+        {
+            if(vis[i] == 0)
+            {
+                cnt++;
+                dfs(i,vis,adj);
             }
         }
-
-        return ans;
+        
+        return cnt;
     }
 };
