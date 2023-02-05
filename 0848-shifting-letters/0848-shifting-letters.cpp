05f981b8ae1fd971e6b0466eb836d18fdeb934f4
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-02-05 15:48:22 +0530

    Time: 135 ms (85.29%), Space: 67.9 MB (68.16%) - LeetHub

---
+++ 0848-shifting-letters/0848-shifting-letters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        
+        int sum = 0;
+        
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            sum = (sum + shifts[i]) % 26;
+            s[i] = (s[i]-97+sum)%26 + 'a';
+        }
+        
+        return s;
+    }
+};
