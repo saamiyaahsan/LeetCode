f7e2fff89ae2c18875d3a508124e6c033f69d0b0
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-03-14 21:29:50 +0530

    Time: 226 ms (45.52%), Space: 41.7 MB (41.94%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
-public:
+public: 
     int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
-        
+          
         map<string,int>mp;
         
         for(int i=0;i<=s.size()-minSize;i++)
@@ -11,10 +11,16 @@ public:
         
         int ans = 0;
         
+        set<char>st;
+        
         for(auto it : mp)
         {
-            set<char> subs(begin(it.first),end(it.first));
-            if(subs.size() <= maxLetters)
+            set<char>st;
+            for(int i=0;i<it.first.size();i++)
+            {
+                st.insert(it.first[i]);
+            }
+            if(st.size() <= maxLetters)
             {
                ans = max(ans,it.second);        
             }
