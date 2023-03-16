64cf1b09eb2b29e056a070c538a36da4d95c6963
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-03-16 23:56:00 +0530

    Time: 173 ms (79.42%), Space: 81.7 MB (63.84%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,21 +1,51 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &graph,int i,int j,vector<vector<int>> &ans){
-        for(auto &x:graph[j]){
-            if(ans[x].empty() || ans[x].back()!=i){
-                ans[x].push_back(i);
-                dfs(graph,i,x,ans);
-            }
-        }
-    }
-    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> ans(n),graph(n);
-        for(auto &v:edges){
-            graph[v[0]].push_back(v[1]);
-        }
-        for(int i=0;i<n;i++){
-            dfs(graph,i,i,ans);
-        }
-        return ans;
+    
+   
+     
+     void bfs(vector<vector<int>>& adj,int node,vector<vector<int>>& ans)
+     {
+         queue<int>q;
+         
+         vector<bool>visited(adj.size(),false);
+         
+         q.push(node);
+         visited[node] = 1;
+         
+         while(q.empty() != true)
+         {
+                 int x = q.front();
+                 q.pop();
+                 for(auto &it : adj[x])
+                {
+                     if(visited[it] == false)
+                    {
+                          visited[it] = true; 
+                       ans[it].push_back(node);
+                       q.push(it);
+                     }
+                } 
+            
+         }
+        
+     }
+     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+      
+     vector<vector<int>>adj(n);   
+     
+     for(int i=0;i<edges.size();i++)   
+     {
+         adj[edges[i][0]].push_back(edges[i][1]);
+     }   
+     
+          vector<vector<int>> ans(n);
+         
+         for(int i=0;i<n;i++)
+         {
+             bfs(adj,i,ans);         
+         }
+         
+         return ans;
+      
     }
 };
