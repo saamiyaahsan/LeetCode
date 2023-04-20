6a1d25e09579224850dccbe84334545f6387a8dc
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-04-21 00:22:37 +0530

    Time: 1016 ms (36.64%), Space: 8.2 MB (25.29%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -3,39 +3,41 @@ public:
     
     bool dfs(vector<vector<char>>& board,int i,int j,int index,string word)
     {
-       if(index == word.size())
-       {
-           return true;
-       }
-        
-        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[index] || board[i][j] == '*')
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[index])
         {
             return false;
         }
         
-        char x = board[i][j];
+        if(index == word.size()-1)
+        {
+            return true;
+        }
         
-        board[i][j] = '#';
+        char X = board[i][j];
         
-        bool left = dfs(board,i-1,j,index+1,word);
-        bool right = dfs(board,i+1,j,index+1,word);
-        bool top = dfs(board,i,j-1,index+1,word);
-        bool bottom = dfs(board,i,j+1,index+1,word);
+        board[i][j] = '0';
         
-        board[i][j] = x;
+        bool x = dfs(board,i+1,j,index+1,word);
+        bool y = dfs(board,i-1,j,index+1,word);
+        bool z = dfs(board,i,j-1,index+1,word);
+        bool w = dfs(board,i,j+1,index+1,word);
         
-        return left || right || top || bottom;
+        board[i][j] = X;
+        
+        return x || y || z || w;
     }
     
     bool exist(vector<vector<char>>& board, string word) {
         
-        for(int i=0;i<board.size();i++)
+        for(int i = 0;i < board.size();i++)
         {
             for(int j=0;j<board[0].size();j++)
             {
                 if(board[i][j] == word[0])
                 {
-                    if(dfs(board,i,j,0,word) == true)
+                    bool x = dfs(board,i,j,0,word);
+                    
+                    if(x == true)
                     {
                         return true;
                     }
@@ -43,7 +45,7 @@ public:
             }
         }
         
-        
         return false;
+        
     }
 };
