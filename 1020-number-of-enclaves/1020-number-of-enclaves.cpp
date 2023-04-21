0ab06f930f3c92d7c000dae27c244625d5d668b3
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-04-21 17:13:09 +0530

    Time: 77 ms (63.18%), Space: 31.2 MB (42.83%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -3,12 +3,12 @@ public:
     
     void dfs(vector<vector<int>>& grid,int i,int j)
     {
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != 1)
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != 1)
         {
             return;
         }
         
-        grid[i][j] = 2;
+        grid[i][j] = 0;
         
         dfs(grid,i-1,j);
         dfs(grid,i+1,j);
@@ -17,7 +17,6 @@ public:
     }
     
     int numEnclaves(vector<vector<int>>& grid) {
-        
         
         for(int i=0;i<grid.size();i++)
         {
