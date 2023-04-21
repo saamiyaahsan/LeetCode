02c31f9315c1874f050c55ea4bd8fd3426d0bc77
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-04-21 16:29:05 +0530

    Time: 4 ms (67.28%), Space: 16.4 MB (84.19%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -1,28 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     
-    bool Mirror(TreeNode* left,TreeNode* right)
+    bool helper(TreeNode* p,TreeNode* q)
     {
-       if(left == NULL || right == NULL)
-       {
-           return right == left;
-       }
-        
-       if(left->val != right->val)
-       {
-           return false;
-       }
-        
-        return Mirror(left->left,right->right) && Mirror(left->right,right->left);
-    }
-    
-    bool isSymmetric(TreeNode* root) {
-        
-        if(root == NULL)
+        if(p == NULL && q == NULL)
         {
             return true;
         }
         
-        return Mirror(root->left,root->right);
+        if(p != NULL && q == NULL || p == NULL && q != NULL)
+        {
+            return false;
+        }
+        
+        if(p->val != q->val)
+        {
+            return false;
+        }
+        
+        return helper(p->left,q->right) && helper(p->right,q->left);
     }
+    
+    bool isSymmetric(TreeNode* root) {
+      
+        if(root == NULL)
+        {
+            return true;
+        }
+         
+        if(root->left == NULL && root->right == NULL)
+        {
+            return true;    
+        }
+        
+        return helper(root->left,root->right);
+        
+        }
 };
