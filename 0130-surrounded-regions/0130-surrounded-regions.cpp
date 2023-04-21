748d264f52909474840652116e0e224f2779f451
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-04-21 17:49:14 +0530

    Time: 11 ms (87.42%), Space: 9.9 MB (89.62%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -8,38 +8,39 @@ public:
             return;
         }
         
-        board[i][j] = 'Y';
+        board[i][j] = 'S';
         
-        dfs(board,i+1,j);
         dfs(board,i-1,j);
+        dfs(board,i+1,j);
         dfs(board,i,j-1);
         dfs(board,i,j+1);
     }
+    
     void solve(vector<vector<char>>& board) {
-        
+       
         for(int i=0;i<board.size();i++)
         {
             for(int j=0;j<board[0].size();j++)
             {
-                if(i == board.size()-1 && board[i][j] == 'O' || i == 0 && board[i][j] == 'O' || j == 0 && board[i][j] == 'O' || j == board[0].size()-1 && board[i][j] == 'O')
+                if((i == 0 || j == 0 || i == board.size()-1 || j == board[0].size()-1) && board[i][j] == 'O')
                 {
                     dfs(board,i,j);
                 }
             }
         }
         
-        for(int i=0;i<board.size();i++)
+        for(int i = 0;i < board.size();i++)
         {
-            for(int j=0;j<board[0].size();j++)
+            for(int j = 0;j < board[0].size();j++)
             {
-                if(board[i][j] == 'Y')
-                {
-                    board[i][j] = 'O';
-                }
-                
-                else if(board[i][j] == 'O')
+                if(board[i][j] == 'O')
                 {
                     board[i][j] = 'X';
+                }
+                
+                else if(board[i][j] == 'S')
+                {
+                    board[i][j] = 'O';
                 }
             }
         }
