76d37e14c623b9457a8ee50b00c1e1b54d728a70
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-04-21 18:10:32 +0530

    Time: 15 ms (83.38%), Space: 28.6 MB (38.08%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -1,39 +1,45 @@
 class Solution {
 public:
     
-    void dfs(vector<vector<int>>& grid,int i,int j,int step,int &cnt)
+    void dfs(vector<vector<int>>& grid,int i,int j,vector<vector<int>>& visited,int step)
     {
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != step + 1)
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || visited[i][j] == 1 || grid[i][j] != step + 1)
         {
             return;
         }
         
-        cnt++;
-        int parent_value = grid[i][j];
-        grid[i][j] =0;
+        visited[i][j] = 1;
         
-        dfs(grid,i+2,j+1,parent_value,cnt);
-        dfs(grid,i+2,j-1,parent_value,cnt);
-        dfs(grid,i-2,j+1,parent_value,cnt);
-        dfs(grid,i-2,j-1,parent_value,cnt);
-        dfs(grid,i+1,j+2,parent_value,cnt);
-        dfs(grid,i+1,j-2,parent_value,cnt);
-        dfs(grid,i-1,j-2,parent_value,cnt);
-        dfs(grid,i-1,j+2,parent_value,cnt);
-        
-        //p || q || r || s || a || b || c || d;
+        dfs(grid,i-2,j-1,visited,step + 1);
+        dfs(grid,i+2,j+1,visited,step + 1);
+        dfs(grid,i-2,j+1,visited,step + 1);
+        dfs(grid,i+2,j-1,visited,step + 1);
+        dfs(grid,i-1,j-2,visited,step + 1);
+        dfs(grid,i+1,j+2,visited,step + 1);
+        dfs(grid,i-1,j+2,visited,step + 1);
+        dfs(grid,i+1,j-2,visited,step + 1);
     }
     bool checkValidGrid(vector<vector<int>>& grid) {
         
-        int cnt = 0,step = -1;
+        int n = grid.size(),m = grid[0].size();
         
-        dfs(grid,0,0,step,cnt);
+        int step = -1;
         
-        if(cnt == grid.size()*grid[0].size())
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        
+        dfs(grid,0,0,visited,step);
+        
+        for(int i=0;i<visited.size();i++)
         {
-            return true;
+            for(int j=0;j<visited[0].size();j++)
+            {
+                if(visited[i][j] == 0)
+                {
+                    return false;
+                }
+            }
         }
         
-        return false;
+        return true;
     }
 };
