23ed442360baeb0df07a212a5bb5dc9344e6d581
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-04-28 20:20:31 +0530

    Time: 12 ms (60.88%), Space: 21.2 MB (76.72%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -13,16 +13,16 @@ class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         
-        if(root == NULL)      
+        if(root == NULL)
         {
             return false;
         }
         
-        if(root->left == NULL && root->right == NULL)
+        if(root->val == targetSum && root->left == NULL && root->right == NULL)
         {
-            return root->val == targetSum;
+            return true;
         }
         
-        return hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val);
+        return hasPathSum(root->left,targetSum - root->val) || hasPathSum(root->right,targetSum - root->val);
     }
 };
