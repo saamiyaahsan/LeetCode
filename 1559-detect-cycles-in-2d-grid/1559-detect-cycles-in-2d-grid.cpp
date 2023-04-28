949dba7917f44b89a67154ac91e2af5b4dee7754
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-04-28 20:16:36 +0530

    Time: 306 ms (40.55%), Space: 58.9 MB (68.23%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    bool dfs(vector<vector<char>>& grid,int i,int j,vector<vector<int>>& visited,char src)
+    bool dfs(vector<vector<char>>& grid,int i,int j,char src,vector<vector<int>>& visited)
     {
         if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != src)
         {
@@ -14,14 +14,13 @@ public:
         }
         
         char temp = grid[i][j];
-        
-        grid[i][j] = '*';
+        grid[i][j] = '#';
         visited[i][j] = 1;
         
-        bool p = dfs(grid,i-1,j,visited,src);
-        bool q = dfs(grid,i+1,j,visited,src);
-        bool r = dfs(grid,i,j-1,visited,src);
-        bool s = dfs(grid,i,j+1,visited,src);
+        bool p = dfs(grid,i-1,j,src,visited);
+        bool q = dfs(grid,i+1,j,src,visited);
+        bool r = dfs(grid,i,j-1,src,visited);
+        bool s = dfs(grid,i,j+1,src,visited);
         
         grid[i][j] = temp;
         
@@ -33,12 +32,13 @@ public:
         
         vector<vector<int>>visited(n,vector<int>(m,0));
         
-        for(int i = 0;i<n;i++)
+        for(int i=0;i<grid.size();i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<grid[0].size();j++)
             {
                 char src = grid[i][j];
-                if(visited[i][j] == 0 && dfs(grid,i,j,visited,src) == true)
+                
+                if(visited[i][j] == 0 && dfs(grid,i,j,src,visited) == true)
                 {
                     return true;
                 }
