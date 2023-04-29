c367b5f3a8b1c6317208846f9c98ced6c5d3121e
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-04-29 15:47:38 +0530

    Time: 19 ms (85.89%), Space: 13.3 MB (82.13%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,49 +2,50 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         
-        queue<int>q;
+        vector<int>indegree(numCourses,0);
         
-        int n = numCourses;
+        vector<int>visited(numCourses,0);
         
-        vector<vector<int>>adj(n);
+        vector<vector<int>>adj(numCourses);
         
-            vector<int>indegree(n,0);
-            
         for(int i=0;i<prerequisites.size();i++)
         {
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
             indegree[prerequisites[i][0]]++;
         }
         
+        queue<int>q;
+        
         for(int i=0;i<indegree.size();i++)
         {
-            if(indegree[i] == 0)
-            {
-                q.push(i);
-            }
+           if(indegree[i] == 0)
+           {
+               q.push(i);
+           }
         }
         
         int cnt = 0;
         
-        while(q.empty() != true)
-        {
-                 int x = q.front();
-                 q.pop();
-                 cnt++;
+             while(q.empty() != true)
+             {
+                int x = q.front();
+                q.pop();
+                
+                cnt++;
+                
                 for(auto it : adj[x])
                 {
-                   indegree[it]--; 
+                    indegree[it]--;
                     
-                   if(indegree[it] == 0)
-                   {
-                     q.push(it);
-                   }
-                
+                    if(indegree[it] == 0)
+                    {
+                        q.push(it);
+                    }
                 }
-           
-        }
+            
+             }
         
-        if(cnt == n)
+        if(cnt == numCourses)
         {
             return true;
         }
