79c2fc5b743f83c29c4e71e6e069088e2a5d13f7
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-05-02 22:41:01 +0530

    Time: 438 ms (15.76%), Space: 8.3 MB (16.00%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,52 +1,41 @@
 class Solution {
 public:
-    
-    int dfs(vector<vector<int>>& grid,int i,int j,int ans,vector<vector<int>>& visited)
+     int dfs(vector<vector<int>>& grid,int i,int j,vector<vector<int>>& visited)
     {
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || visited[i][j] == 1 || grid[i][j] == 0)
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0 || visited[i][j] == 1)
         {
-            return 0;    
+            return 0;
         }
         
         visited[i][j] = 1;
         
-        ans = grid[i][j] + max({dfs(grid,i,j-1,ans,visited),dfs(grid,i,j+1,ans,visited),dfs(grid,i-1,j,ans,visited),dfs(grid,i+1,j,ans,visited)});
+        int ans = grid[i][j] + max({dfs(grid,i,j-1,visited),dfs(grid,i,j+1,visited),dfs(grid,i-1,j,visited),dfs(grid,i+1,j,visited)});
         
-        visited[i][j] = 0;
-        
+         visited[i][j] = 0;  
+         
         return ans;
-    }
+      
+     }
     int getMaximumGold(vector<vector<int>>& grid) {
         
-//         vector<vector<int>>visited;
-        
-//         for(int i=0;i<visited.size();i++)
-//         {
-//             for(int j=0;j<visited[0].size();j++)
-//             {
-//                 visited.push_back(0);
-//             }
-//         }
-        
-        int n = grid.size(), m = grid[0].size();
-        
-        int ans = 0;
+        int ans = INT_MIN;
         
         for(int i=0;i<grid.size();i++)
         {
             for(int j=0;j<grid[0].size();j++)
             {
-                vector<vector<int>>visited(n,vector<int>(m));
-                
                 if(grid[i][j] != 0)
                 {
-                   int p = dfs(grid,i,j,ans,visited);   
-                   ans = max(ans,p);       
+                    vector<vector<int>>visited(grid.size(),vector<int>(grid[0].size(),0));
+                    ans = max(ans,dfs(grid,i,j,visited));
                 }
-                
             }
         }
         
+        if(ans == INT_MIN)
+        {
+            return 0;
+        }
         return ans;
     }
 };
