8ea3bdb9e914433615c17941ade4697d70599bf9
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-05-04 12:44:43 +0530

    Time: 10 ms (70.34%), Space: 11.9 MB (70.36%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,54 +1,61 @@
 class Solution {
 public:
     
-    long helper(int a,int b,char op)
-    {
-        if(op == '+')
-        {
-            return a+b;
-        }
-        
-        else if(op == '-')
-        {
-            return a-b;
-        }
-        
-        else if(op == '*')
-        {
-            return (long)a*b;
-        }
-        
-        else
-        {
-            return a/b;
-        }
-    }
+    
+    
     int evalRPN(vector<string>& tokens) {
-        
-        stack<long>st;
+       
+        stack<int>st;
         
         for(int i=0;i<tokens.size();i++)
         {
-            if(tokens[i].size() == 1 && tokens[i][0] < 48)
+            if(tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/")
             {
-                int p = st.top();
-                
-                st.pop();
-                
-                int q = st.top();
-                
-                st.pop();
-                
-                string oper = tokens[i];
-                
-                long x = helper(q,p,oper[0]);
-                
-                st.push(x);
+                int num = stoi(tokens[i]);
+                st.push(num);
             }
             
             else
             {
-                st.push(stoi(tokens[i]));
+                if(tokens[i] == "+")
+                {
+                    int x = st.top();
+                    st.pop();
+                    int y = st.top();
+                    st.pop();
+                    int z = x + y;
+                    st.push(z);
+                }
+                
+                else if(tokens[i] == "-")
+                {
+                    int x = st.top();
+                    st.pop();
+                    int y = st.top();
+                    st.pop();
+                    int z = y - x;
+                    st.push(z); 
+                }
+                
+                else if(tokens[i] == "*")
+                {
+                    int x = st.top();
+                    st.pop();
+                    int y = st.top();
+                    st.pop();
+                    int z = x*y;
+                    st.push(z); 
+                }
+                
+                else
+                {
+                    int x = st.top();
+                    st.pop();
+                    int y = st.top();
+                    st.pop();
+                    int z = y/x;
+                    st.push(z);  
+                }
             }
         }
         
