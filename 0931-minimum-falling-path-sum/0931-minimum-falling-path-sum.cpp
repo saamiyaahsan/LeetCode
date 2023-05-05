2ab01dc00a04e08018eb94028646cc357945be3c
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-05-05 20:01:58 +0530

    Time: 13 ms (79.93%), Space: 10.2 MB (60.11%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -8,19 +8,24 @@ public:
         
         dp[0]=matrix[0];
         
-        for(int i=1;i<n;i++){
-            for(int j=0;j<m;j++){
-			    // min of up and up-right
-                if(j==0){
+        for(int i=1;i<n;i++)
+        {
+             for(int j=0;j<m;j++)
+             {
+			    
+                if(j==0)
+                {
                     dp[i][j]=matrix[i][j]+min(dp[i-1][j], dp[i-1][j+1]);
                 }
-				// min of up and up-left
-                else if(j==m-1){
+				
+                else if(j==m-1)
+                {
                     dp[i][j]=matrix[i][j]+min(dp[i-1][j], dp[i-1][j-1]);
                 }
-				// min of up, up-right and up-left
-                else{
-                    dp[i][j]=matrix[i][j]+min(dp[i-1][j], min(dp[i-1][j+1], dp[i-1][j-1]));
+				
+                else
+                {
+                    dp[i][j]=matrix[i][j]+min({dp[i-1][j], dp[i-1][j+1], dp[i-1][j-1]});
                 }
             }
         }
