ac849483a57cdeaecff666bb505b8f5495174cfd
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-05-27 19:40:08 +0530

    Time: 4 ms (48.00%), Space: 7.8 MB (38.55%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-         
-        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
         
-        vector<vector<int>> dp(m, vector<int>(n,-1));
+        vector<vector<int>>dp(obstacleGrid.size(),vector<int>(obstacleGrid[0].size(),0));
         
-        for (int i = 0; i < m; i++)
+        for(int i=0;i<obstacleGrid.size();i++)
         {
-            for (int j = 0; j < n; j++)
+            for(int j=0;j<obstacleGrid[0].size();j++)
             {
-                if (obstacleGrid[i][j] == 1) 
+                if(obstacleGrid[i][j] == 1)
                 {
                     dp[i][j] = 0;
-                    continue;
                 }
                 
-                if (i == 0 && j == 0) 
+                else
                 {
-                    dp[i][j] = 1;
-                    continue;
+                    if(i == 0 && j == 0)
+                    {
+                        dp[i][j] = 1;
+                    }
+                    
+                    else if(i == 0 && j != 0)
+                    {
+                        dp[i][j] = dp[i][j-1];
+                    }
+                    
+                    else if(i != 0 && j == 0)
+                    {
+                        dp[i][j] = dp[i-1][j];
+                    }
+                    else
+                    {
+                        dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                    }
                 }
-                
-                int left = 0, up = 0;
-                if (j > 0)
-                    left = dp[i][j-1];
-                if (i > 0)
-                    up = dp[i-1][j];    
-                dp[i][j] = left + up;
             }
         }
         
-        return dp[m-1][n-1];
+        return dp[obstacleGrid.size()-1][obstacleGrid[0].size()-1];
     }
 };
