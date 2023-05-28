c3cac86e8b508223ca93fefac88201d207790fa1
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-05-28 16:07:01 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (6.54%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    
+    int helper(int n,vector<int>& nums,vector<int>& dp)
+    {
+        if(n == 0)
+        {
+            return nums[0];
+        }
+        
+        if(n == 1)
+        {
+            return max(nums[0],nums[1]);
+        }
+        
+        if(dp[n] != -1)
+        {
+            return dp[n];
+        }
+        
+        dp[n] = max(helper(n-1,nums,dp),nums[n]+helper(n-2,nums,dp));
+        return dp[n];
+    }
+    
     int rob(vector<int>& nums) {
         
         if(nums.size() == 1)
@@ -7,31 +29,42 @@ public:
             return nums[0];
         }
         
-        if(nums.size() == 2)
+        vector<int>a;
+        vector<int>b;
+        
+        vector<int>dp1(nums.size()-1,-1);
+        vector<int>dp2(nums.size()-1,-1);
+        
+        for(int i=0;i<nums.size()-1;i++)
         {
-            return max(nums[0],nums[1]);
+            a.push_back(nums[i]);
         }
         
-        vector<int>dp1(nums.size()-1,0);
-        vector<int>dp2(nums.size(),0);
+        for(int i=1;i<nums.size();i++)
+        {
+            b.push_back(nums[i]);
+        }
         
         dp1[0] = nums[0];
-        dp1[1] = max(nums[0],nums[1]);
         
-        for(int i=2;i<nums.size()-1;i++)
+        if(nums.size() > 2)
         {
-            dp1[i] = max(dp1[i-1],dp1[i-2]+nums[i]);
+            dp1[1] = max(nums[0],nums[1]);    
         }
         
-        dp2[0] = 0;
-        dp2[1] = nums[1];
-        dp2[2] = max(nums[1],nums[2]);
-        
-        for(int i=3;i<nums.size();i++)
+        if(nums.size() > 1)
         {
-            dp2[i] = max(dp2[i-1],dp2[i-2]+nums[i]);
+            dp2[0] = nums[1];    
         }
         
-        return max(dp1[nums.size()-2],dp2[nums.size()-1]);
+        if(nums.size() >= 3)
+        {
+            dp2[1] = max(nums[1],nums[2]);     
+        }
+       
+        helper(a.size()-1,a,dp1);
+        helper(b.size()-1,b,dp2);
+        
+        return max(dp1[nums.size()-2],dp2[nums.size()-2]);
     }
 };
