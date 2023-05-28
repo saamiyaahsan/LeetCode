58d1ebd0a08998897095a065138af4095960d84e
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-05-28 15:18:18 +0530

    Time: 6 ms (10.04%), Space: 7.7 MB (53.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        
-        vector<int>dp(nums.size(),0);
-        
-        if(nums.size() == 1)
+    
+    int helper(int n,vector<int>& nums,vector<int>& dp)
+    {
+        if(n == 0)
         {
             return nums[0];
         }
         
-        if(nums.size() == 2)
+        if(n == 1)
         {
             return max(nums[0],nums[1]);
         }
-        dp[0] = nums[0];
         
-        dp[1] = max(nums[0],nums[1]);
-        
-        for(int i=2;i<nums.size();i++)
+        if(dp[n] != -1)
         {
-            dp[i] = max(dp[i-1],dp[i-2] + nums[i]);
+            return dp[n];
         }
         
-        return dp[nums.size()-1];
+        dp[n] = max(helper(n-1,nums,dp),nums[n] + helper(n-2,nums,dp));
+        
+        return dp[n];
+    }
+    int rob(vector<int>& nums) {
+        
+        vector<int>dp(nums.size(),-1);
+        
+        int n = nums.size();
+        
+        if(nums.size() == 1)
+        {
+           dp[0] = nums[0];    
+        }
+        
+        if(nums.size() == 2)
+        {
+            dp[0] = nums[0];
+            dp[1] = max(nums[0],nums[1]);
+        }
+        
+        helper(n-1,nums,dp);
+        
+        
+        return dp[n-1];
     }
 };
