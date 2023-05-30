ea74d50b30b8b28947e78e757528682eccaaef21
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-05-30 15:21:33 +0530

    Time: 13 ms (7.47%), Space: 9 MB (51.22%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int helper(vector<vector<int>>& triangle,int i,int j,vector<vector<int>>& dp)
+    int helper(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp)
     {
         if(i == triangle.size()-1)
         {
@@ -13,19 +13,19 @@ public:
             return dp[i][j];
         }
         
-        int down = triangle[i][j] + helper(triangle,i+1,j,dp);
-        int diagonal = triangle[i][j] + helper(triangle,i+1,j+1,dp);
-         
-        return dp[i][j] = min(down,diagonal);
+        int down = triangle[i][j] + helper(i+1,j,triangle,dp);
+        int diagonal = triangle[i][j] + helper(i+1,j+1,triangle,dp);
+        
+        dp[i][j] = min(down,diagonal);
+        
+        return dp[i][j];
     }
-    
     int minimumTotal(vector<vector<int>>& triangle) {
         
         int n = triangle.size();
         
         vector<vector<int>>dp(n,vector<int>(n,-1));
         
-        return helper(triangle,0,0,dp);
-        
+        return helper(0,0,triangle,dp);
     }
 };
