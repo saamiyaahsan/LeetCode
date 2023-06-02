74e44997d4cc5da7fd0e539ec23d9a4e5040cbe0
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-06-02 22:33:21 +0530

    Time: 977 ms (5.00%), Space: 18.1 MB (5.57%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        
-        int pos = 0,destination = 0,cnt = 0;
-         
-        
-        for(int i=0;i<nums.size()-1;i++)
+    
+    int helper(vector<int>& nums,int ind,vector<int>& dp)
+    {
+        if(ind >= nums.size())
         {
-            destination = max(destination,i+nums[i]);
-            
-            if(pos == i)
-            {
-                pos = destination;
-                cnt++;
-            }
+            return INT_MAX;
         }
         
-        return cnt;
+        if(ind == nums.size()-1)
+        {
+            return 0;
+        }
+        
+        if(dp[ind] != INT_MAX)
+        {
+            return dp[ind];
+        }
+        
+        for(int k = ind + 1;k <= ind + nums[ind];k++)
+        {
+            if(helper(nums,k,dp) != INT_MAX)
+            {
+                int x = 1 + helper(nums,k,dp);
+                dp[ind] = min(x,dp[ind]);     
+            }
+           
+        }
+        
+        return dp[ind];
+    }
+    
+    int jump(vector<int>& nums) {
+        
+        vector<int>dp(nums.size(),INT_MAX);
+        
+        return helper(nums,0,dp);
     }
 };
