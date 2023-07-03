050523585893e5acf588eb928aa5a019a8f9664a
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-07-03 15:43:24 +0530

    Time: 49 ms (75.28%), Space: 18.9 MB (79.45%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -30,7 +30,7 @@ public:
          
         if(func(piles,mid) <= h)
         {
-            // ans = func(piles,mid);
+            ans = mid;
             high = mid-1;
         }
          
@@ -40,7 +40,7 @@ public:
         }
      }
         
-        return low;
+        return ans;
         
     }
 };
