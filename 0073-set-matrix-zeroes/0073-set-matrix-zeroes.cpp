1795367b4d2fbd2bd8fca78bf4e9a7310a916775
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-07-22 00:21:37 +0530

    Time: 16 ms (71.18%), Space: 13.2 MB (87.62%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -11,15 +11,16 @@ public:
             {
                 if(matrix[i][j] == 0)
                 {
-                   row[i] = 1;
-                   col[j] = 1; 
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
         
-        for(int i=0;i<row.size();i++)
+        
+        for(int i=0;i<matrix.size();i++)
         {
-            for(int j=0;j<col.size();j++)
+            for(int j=0;j<matrix[0].size();j++)
             {
                 if(row[i] == 1 || col[j] == 1)
                 {
