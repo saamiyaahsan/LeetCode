60737af41735e6c5c6a44dd27cee22bccec5c2f6
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-07-22 00:22:45 +0530

    Time: 8 ms (31.88%), Space: 12 MB (60.27%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         
-        next_permutation(nums.begin(),nums.end());
+      next_permutation(nums.begin(),nums.end());
         
     }
 };
