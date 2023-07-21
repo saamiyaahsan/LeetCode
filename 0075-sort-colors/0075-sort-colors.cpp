c2d783fe5c26293cfc0abe54eb7e99aa382f9172
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-07-22 00:27:46 +0530

    Time: 3 ms (47.50%), Space: 8.2 MB (38.33%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        priority_queue<int,vector<int>,greater<int>>pq;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            pq.push(nums[i]);
+        }
+        
+        nums.clear();
+        
+        while(pq.empty() != true)
+        {
+            nums.push_back(pq.top());
+            pq.pop();
+        }
+        
+    }
+};
