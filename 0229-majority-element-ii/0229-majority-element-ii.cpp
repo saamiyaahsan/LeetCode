b0fdad2e36c5f168451af3af1d7903e3fa0c4f24
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-07-22 11:30:27 +0530

    Time: 16 ms (55.13%), Space: 16 MB (25.62%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,7 +3,8 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         
         vector<int>res;
-        unordered_map<int,int>mp;
+        
+        map<int,int>mp;
         
         for(int i=0;i<nums.size();i++)
         {
