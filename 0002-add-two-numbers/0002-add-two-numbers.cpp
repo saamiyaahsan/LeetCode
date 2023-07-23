6b64c084027b1ea0b9dd0a7dfe8c25f16834a261
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-07-23 12:00:47 +0530

    Time: 28 ms (92.48%), Space: 71.4 MB (25.61%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -12,80 +12,53 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-        vector<int> res;
-        vector<int> ans;
-        vector<int> var;
-        ListNode* p;
-        p = l1;
+        ListNode* p = l1;
+        ListNode* q = l2;
+        
+        ListNode* Res = new ListNode(-1);
+        ListNode* t = Res;
+        int carry = 0;
+        
+        while(p != NULL && q != NULL)
+        {
+            int nodeSum = (p->val+q->val+carry)%10;
+            carry = (p->val+q->val+carry)/10;
+            ListNode* newNode = new ListNode(nodeSum);
+            t->next = newNode;
+            t = t->next;
+            p = p->next;
+            q = q->next;
+        }
         
         while(p != NULL)
         {
-            res.push_back(p->val);
+            int nodeSum = (p->val + carry)% 10;
+            carry = (p->val+carry)/10;
+            ListNode* newNode = new ListNode(nodeSum);
+            t->next = newNode;
+            t = t->next;
             p = p->next;
         }
         
-        reverse(res.begin(),res.end());
-        
-        p = l2;
-        
-        while(p != NULL)
+         while(q != NULL)
         {
-            ans.push_back(p->val);
-            p = p->next;
-        }
-        
-        reverse(ans.begin(),ans.end());
-        
-        int i,j,carry=0,x;
-        
-        i = res.size()-1;
-        j = ans.size()-1;
-        
-        while(i >= 0 && j >= 0)
-        {
-            x = res[i] + ans[j] + carry;
-            carry = x/10;
-            x = x%10;
-            var.push_back(x);
-            i--;
-            j--;
-        }
-        
-        while(i >= 0)
-        {
-            x = res[i] + carry;
-            carry = x/10;
-            x = x%10;
-            var.push_back(x);
-            i--;
-        }
-        
-        while(j >= 0)
-        {
-            x = ans[j] + carry;
-            carry = x/10;
-            x = x%10;
-            var.push_back(x);
-            j--;
+            int nodeSum = (q->val + carry)% 10;
+            carry = (q->val+carry)/10;
+            ListNode* newNode = new ListNode(nodeSum);
+            t->next = newNode;
+            t = t->next;
+            q = q->next;
         }
         
         if(carry == 1)
         {
-           var.push_back(carry);    
+            int nodeSum = (carry) % 10;
+            carry = carry/10;
+            ListNode* newNode = new ListNode(nodeSum);
+            t->next = newNode;
+            t = t->next;
         }
         
-        // reverse(var.begin(),var.end());
-        
-        ListNode* temp = new ListNode();
-        ListNode* q = temp;
-        
-        for(i=0;i<var.size();i++)
-        {
-            ListNode* curr = new ListNode(var[i]);
-            q->next = curr;
-            q = q->next;
-        }
-        
-        return temp->next;
+        return Res->next;
     }
 };
