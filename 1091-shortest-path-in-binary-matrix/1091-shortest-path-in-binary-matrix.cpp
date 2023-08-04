e81b99ac36c7be5a0378e53f8473689bcb500fa6
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-08-05 00:28:36 +0530

    Time: 56 ms (94.68%), Space: 20.8 MB (66.67%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         
         queue<pair<pair<int,int>,int>>q;
         
         if(grid.size() == 1 && grid[0].size() == 1 && grid[0][0] == 0)
         {
-           return 1;    
+            return 1;
         }
         
         if(grid[0][0] == 1 || grid[grid.size()-1][grid[0].size()-1] == 1)
@@ -17,37 +16,31 @@ public:
         
         q.push({{0,0},1});
         
-        vector<int>drow = {0,0,1,1,1,-1,-1,-1};
-        vector<int>dcol = {1,-1,0,1,-1,0,1,-1};
+        vector<int>drow = {-1,-1,-1,0,0,1,1,1};
+        vector<int>dcol = {-1,0,1,-1,1,-1,0,1};
         
         while(q.empty() != true)
         {
-            int n = q.size();
+            auto x = q.front();
+            int row = x.first.first;
+            int col = x.first.second;
+            int dist = x.second;
+            q.pop();
             
-            for(int i=0;i<n;i++)
+            for(int i=0;i<drow.size();i++)
             {
-                auto x = q.front();
-                q.pop();
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
                 
-                int row = x.first.first;
-                int col = x.first.second;
-                int dist = x.second;
-                
-                for(int idx = 0;idx < drow.size();idx++)
+                if(nrow >= 0 && nrow <= grid.size()-1 && ncol >= 0 && ncol <= grid[0].size()-1 && grid[nrow][ncol] == 0)
                 {
-                    int nrow = row + drow[idx];
-                    int ncol = col + dcol[idx];
-                    
-                    if(nrow >= 0 && nrow <= grid.size()-1 && ncol >= 0 && ncol <= grid[0].size()-1 && grid[nrow][ncol] == 0)
-                    {
-                        grid[nrow][ncol] = 1;
-                        q.push({{nrow,ncol},dist+1});
-                    }
-                    
-                    if(nrow == grid.size()-1 && ncol == grid[0].size()-1)
-                    {
-                        return dist + 1;
-                    }
+                    grid[nrow][ncol] = 1;
+                    q.push({{nrow,ncol},dist+1});
+                }
+                
+                if(nrow == grid.size()-1 && ncol == grid[0].size()-1)
+                {
+                    return dist + 1;
                 }
             }
         }
