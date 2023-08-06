926234559236e50c2d376e3a688e1e5700b15ce1
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-08-06 16:09:20 +0530

    Time: 6 ms (98.73%), Space: 9.5 MB (63.67%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    void dfs(vector<vector<int>>& grid,int i,int j)
+    {
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != 0)
+        {
+            return;
+        }
+        
+        grid[i][j] = 2;
+        
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+    }
+    int closedIsland(vector<vector<int>>& grid) {
+        
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j] == 0 && (i == 0 || i == grid.size()-1 || j == 0 || j == grid[0].size()-1))
+                {
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        
+        int cnt = 0;
+        
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j] == 0)
+                {
+                    cnt++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        
+        return cnt;
+    }
+};
