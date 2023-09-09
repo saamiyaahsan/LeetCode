a6aef7b0475e558f8b1d4504d3a0b989b2a49fc2
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-09-10 00:23:24 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (85.45%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     
-    int solvememo(vector<int>& nums,int target,vector<int>& dp)
+    int helper(vector<int>& nums,int target,vector<int>& dp)
     {
         if(target == 0)
+        {
             return 1;
+        }
+        
         if(target < 0)
+        {
             return 0;
+        }
         
         if(dp[target] != -1)
+        {
             return dp[target];
-
-        int ans = 0;
-        for(int i = 0; i < nums.size(); i++){
-            ans += solvememo(nums, target - nums[i], dp);
         }
+        
+        int ans = 0;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            ans = ans + helper(nums,target-nums[i],dp);
+        }
+        
         dp[target] = ans;
+        
         return dp[target];
     }
-       
+    
     int combinationSum4(vector<int>& nums, int target) {
-        
+      
         vector<int>dp(target+1,-1);
         
-        return solvememo(nums,target,dp);
+        return helper(nums,target,dp);
     }
 };
