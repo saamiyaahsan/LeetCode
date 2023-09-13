6fc63f379778b74872dba3dab4c363f9503e9881
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-09-13 15:28:19 +0530

    Time: 411 ms (56.29%), Space: 12.5 MB (58.68%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
-    int lenLongestFibSubseq(vector<int>& A) {
+    int lenLongestFibSubseq(vector<int>& arr) {
         
-         int N = A.size();
-        unordered_set<int> S(A.begin(), A.end());
-
+        int N = arr.size();
+        
         int ans = 0;
-        for (int i = 0; i < N; ++i)
-            for (int j = i+1; j < N; ++j) {
-                /* With the starting pair (A[i], A[j]),
-                 * y represents the future expected value in
-                 * the fibonacci subsequence, and x represents
-                 * the most current value found. */
-                int x = A[j], y = A[i] + A[j];
+        
+        unordered_set<int>st(arr.begin(),arr.end());
+        
+        for(int i=0;i<N;++i)
+        {
+            for(int j=i+1;j<N;++j)
+            {
+                int x = arr[i];
+                int y = arr[j];
+                int z = arr[i] + arr[j];
                 int length = 2;
-                while (S.find(y) != S.end()) {
-                    int z = x + y;
+                
+                while(st.find(z) != st.end())
+                {
                     x = y;
                     y = z;
-                    ans = max(ans, ++length);
+                    z = x+y;
+                    length++;
                 }
+                
+               ans = max(ans,length);
             }
-
-        return ans >= 3 ? ans : 0;
+        }
         
+        if(ans >= 3)
+        {
+            return ans;
+        }
+        
+        return 0;
     }
 };
